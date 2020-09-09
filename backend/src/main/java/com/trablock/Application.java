824ec428874a8cebf585e2cda39d331aa9babe12
kjy951207 <kjy951207@naver.com>
2020-09-09 16:07:43 +0900

    [ADD] mapper config, user mapper #S03P22A101-24

--- backend/src/main/java/com/trablock/Application.java
+++ backend/src/main/java/com/trablock/Application.java
@@ -1,13 +1,15 @@
 package com.trablock;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(value = "com.trablock.domain.repository")
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
+ 
 }
