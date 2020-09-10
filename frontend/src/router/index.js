f22d8c9a958f68a3c2f77bd686ea9b8158cf99d9
김민지 <kimin0412@gmail.com>
2020-09-10 16:32:51 +0900

    [UPDATE] Main.vue Added, App.vue Updated

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Main from "../views/Main.vue";
 
 Vue.use(VueRouter)
 
   const routes = [
+    {
+      path: "/",
+      name: "main",
+      component: Main,
+    },
   ]
 
 const router = new VueRouter({
