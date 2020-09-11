bd291a0113bde75fb21d0e667c67492dd7777999
김민지 <kimin0412@gmail.com>
2020-09-11 11:14:33 +0900

    [UPDATE] Main.vue - Updated Background (Resolves S03P22A101-69)

--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -1,6 +1,6 @@
-// import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/css/all.css'
 // import 'material-design-icons-iconfont/dist/material-design-icons.css'
-// import '@mdi/font/css/materialdesignicons.css'
+import '@mdi/font/css/materialdesignicons.css'
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
 
