1aef7626416daffb00f575f9448254b2e3eb4d7e
김민지 <kimin0412@gmail.com>
2020-10-07 22:31:01 +0900

    [UPDATE] MyPage Modified

--- frontend/src/views/group/GroupMain.vue
+++ frontend/src/views/group/GroupMain.vue
@@ -115,11 +115,6 @@
                                 <v-col cols="7" class="text-right py-1 pr-5">{{currentUser.nickname}}</v-col>
                             </v-row>
                             <v-divider></v-divider>
-                            <v-row>
-                                <v-col cols="5" class="text-left py-1 pl-5 pr-0">가입일</v-col>
-                                <v-col cols="7" class="text-right py-1 pr-5 pl-0">{{U.created}}</v-col>
-                            </v-row>
-                            <v-divider></v-divider>
                         </v-card-text>
                         <v-card-actions class="d-flex justify-end pt-0">
                             <v-btn color="blue" text small class="font-weight-bold" @click.stop="dialog = true">update</v-btn>
@@ -176,35 +171,71 @@
                     <v-card class="mx-auto" height="71vh">
                       <v-container>
                         <v-row>
-            <v-col cols="12" class="py-1 text-h5">PROFILE</v-col>
-            <v-col cols="12" class="py-1 text-h4 font-weight-bold">MY Wallet</v-col>
-          </v-row>
+            <v-col cols="12" class="pt-5 pb-0 text-center text-h3 font-weight-bold"><div>{{currentUser.nickname}}의 PROFILE</div></v-col>
+            <v-col cols="12" class="py-1 text-h4 font-weight-bold"><div>MY</div>
+            <!-- <v-icon class="font-weight-bold" fab x-large dark>fas fa-wallet</v-icon> -->
+              </v-col>
+            </v-row>
 
-          
-          <v-row class="mt-10">
-            <v-col cols="12" class="py-1 text-h6">wallet 이름</v-col>
-            <v-col cols="4" class="ml-1 mt-3">
-                <img src="@/assets/user.png" alt="user-image" class="profile-image">
-            </v-col>
-            <br>
-            <v-col cols="12">
-              <v-simple-table dense>
-                <template v-slot:default>
-                  <tbody>
-                  <tr>
-                      <td>지갑 주소</td>
-                      <td>{{Wallet.address}}</td>
-                    </tr>
-                    <tr>
-                      <td>잔액</td>
-                      <td>{{Wallet.balance}}</td>
-                      <td><v-btn @click="charge">충전</v-btn></td>
-                    </tr>
-                  </tbody>
+            <v-row>
+              <v-col cols="12">
+                <template>
+                  <v-card
+                    class="mx-auto"
+                    color="green"
+                    dark
+                    max-width="650"
+                    elevation="10"
+                  >
+                    <v-card-title>
+                      <v-icon
+                        large
+                        left
+                      >
+                        fas fa-wallet
+                      </v-icon>
+                      <span class="title font-weight-light">내 지갑</span>
+                    </v-card-title>
+
+                    <v-card-text class="headline font-weight-light">
+                      <v-col cols="12" class="display-0" align="left">
+                        {{Wallet.address}}
+                      </v-col>
+                      <!-- <td>{{Wallet.address}}</td> -->
+                    </v-card-text>
+                    <v-card-text class="headline font-weight-bold">
+                      <v-row align="center">
+                        <v-col cols="3" class="display-0.5" align="left">
+                          남은 잔액
+                        </v-col>
+                        <v-col cols="9" class="display-3" align="right">
+                          {{ (Number(Wallet.balance) / 1000000000000000000).toFixed(9) }}
+                        </v-col>
+                      </v-row>
+                    </v-card-text>
+
+                    <v-card-actions>
+                      <v-list-item class="grow">
+                        <v-row
+                          align="center"
+                          justify="end"
+                        >
+                          <v-icon class="mr-1" @click="charge">
+                            mdi-heart
+                          </v-icon>
+                          <span class="subheading mr-2">충전하기</span>
+                          <!-- <span class="mr-1">·</span>
+                          <v-icon class="mr-1">
+                            mdi-share-variant
+                          </v-icon>
+                          <span class="subheading">45</span> -->
+                        </v-row>
+                      </v-list-item>
+                    </v-card-actions>
+                  </v-card>
                 </template>
-              </v-simple-table>              
-            </v-col>
-          </v-row>
+              </v-col>
+            </v-row>
                       </v-container>
                     </v-card>
                 </v-col>
